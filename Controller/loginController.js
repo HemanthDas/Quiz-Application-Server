a144951016069f5367f023c6ff09b55f48297336
HemanthDas <hemanthchoudary4@gmail.com>
2024-01-09 15:38:46 +0530

    Update: JWT token added

--- Controller/loginController.js
+++ Controller/loginController.js
@@ -1,9 +1,9 @@
 const User = require("../model/User");
+const generateToken = require("../service/authService");
 const loginController = async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await User.findOne({ username });
-    console.log(user);
     if (!user) {
       res.status(404).json({
         status: "error",
@@ -20,8 +20,14 @@ const loginController = async (req, res) => {
     res.status(200).json({
       status: "success",
       message: "User logged in successfully",
-      token: "token",
+      token: await generateToken(user),
     });
-  } catch {}
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
 };
 module.exports = loginController;
--- service/authService.js
+++ service/authService.js
@@ -1,13 +1,15 @@
+const jwt = require("jsonwebtoken");
 const generateToken = (user) => {
   const token = jwt.sign(
     {
       id: user._id,
       username: user.username,
     },
-    process.env.JWT_SECRET,
+    process.env.SECRET_KEY,
     {
       expiresIn: "30d",
     }
   );
   return token;
 };
+module.exports = generateToken;
